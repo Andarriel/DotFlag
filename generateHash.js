9c090899788c6f8d6890fcac3a85756190d055f1
Vlanatex <vlanatex404@gmail.com>
2026-02-15 21:14:04 +0200

    chore: remove dist directory from .gitignore

--- generateHash.js
+++ generateHash.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 
-const password = 'ADMINISTRATOR!@#';
+const password = 'ivan123';
 const saltRounds = 10;
 
 bcrypt.hash(password, saltRounds, function(err, hash) {
