68aff7d851516cfd4ea3e725df02c48c180045c7
Zulthrex <papichb@yandex.ru>
2026-04-16 21:33:05 +0300

    feat: add CtfEvent API controller with status and update endpoints

---
+++ backend/DotFlag.Api/Controller/CtfEventController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotFlag.Api.Controller
+{
+    [Route("api/ctf")]
+    [ApiController]
+    public class CtfEventController : ControllerBase
+    {
+
+    }
+}
